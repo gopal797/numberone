ae24b436407c6c83069b4f03e8e21ff663f2d16e
gopal797 <63120407+gopal797@users.noreply.github.com>
2020-04-07 10:48:09 +0530

    subtwo

--- views.py
+++ views.py
@@ -25,3 +25,8 @@ print("This is Gopal first branch")
 
 print("This is Gopal first branch")
 print("Thanks")
+
+
+creating new sub two branch
+In Informatics, dummy data is benign information that does not contain any useful data, but serves to reserve space where real data is nominally present. 
+Dummy data can be used as a placeholder for both testing and operational purposes.
